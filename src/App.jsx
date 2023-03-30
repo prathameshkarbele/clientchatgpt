77dbcf3e5a7288f1a3fca0497c741724dd18d7fe
Prathamesh karbele <113411026+prathameshkarbele@users.noreply.github.com>
2023-03-30 20:03:47 +0530

    p1

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import ChatgptBody from "./Componant/ChatgptBody";
+import ChatgptInput from "./Componant/ChatgptInput";
+
+import { useMutation } from "react-query";
+import { useState } from "react";
+import { fetchResponse } from "./api"
+
+function App() {
+  const [chat, setChat] = useState([]);
+
+  const mutation = useMutation({
+    mutationFn: () => {
+      return fetchResponse(chat);
+    },
+    onSuccess: (data) =>
+      setChat((prev) => [
+        ...prev,
+        { sender: "ai", message: data.message.replace(/^\n\n/, "") },
+      ]),
+  });
+  const sendMeassage = async (message) => {
+    await Promise.resolve(setChat((prev) => [...prev, message]));
+    mutation.mutate();
+  };
+  return (
+    <div className="bg-[#1A232E] h-screen py-6 relative sm:px-16 px-12 text-white overflow-hidden flex flex-col justify-between align-middle">
+      <div className="gradient-01 z-0 absolute"></div>
+      <div className="gradient-02 z-0 absolute"></div>
+
+      <div className="uppercase font-bold text-3xl text-center mb-3">
+        ChatGPT@2.0
+      </div>
+
+      <div
+        className="h-[90%] overflow-auto w-full max-w-4xl min-w-[20rem] py-8 px-4 self-center
+      scrollbar-thumb-slate-400 scrollbar-thin scrollbar-track-gray-transparent scrollbar-thumb-rounded-md
+      "
+      >
+        <ChatgptBody  chat={chat}/>
+      </div>
+
+      <div className="w-full max-w-4xl min-w-[20rem] self-center">
+        <ChatgptInput sendMeassage={sendMeassage} loading={mutation.isLoading}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+ //chat={chat}
+ //sendMeassage={sendMeassage} loading={mutation.isLoading}
